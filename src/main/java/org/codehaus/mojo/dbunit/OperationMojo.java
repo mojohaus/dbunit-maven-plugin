01863cecac09413a9a443e4a1ba3f0ea159201ef
Dan Tran <dantran@gmail.com>
2007-01-06 09:48:25 +0000

    combine all import mojos into one to take advantage of the logic already provided by dbunit's ant task, will deprecate existing mojos later
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/mojo-sandbox/dbunit-maven-plugin@2992 52ab4f32-60fc-0310-b215-8acea882cd1b

---
+++ src/main/java/org/codehaus/mojo/dbunit/OperationMojo.java
@@ -0,0 +1,88 @@
+package org.codehaus.mojo.dbunit;
+
+import java.io.File;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.dbunit.ant.Operation;
+import org.dbunit.database.IDatabaseConnection;
+
+/**
+ * Execute DBUnit Operation
+ * 
+ * @goal operation
+ * @author <a href="mailto:dantran@gmail.com">Dan Tran</a>
+ * @author <a href="mailto:topping@codehaus.org">Brian Topping</a>
+ * @version $Id: $
+ * 
+ */
+public class OperationMojo
+    extends AbstractDbUnitMojo
+{
+    /**
+     * Type of Database operation to perform. Supported types are UPDATE, 
+     * INSERT, DELETE, DELETE_ALL, REFRESH, CLEAN_INSERT, MSSQL_INSERT, 
+     * MSSQL_REFRESH, MSSQL_CLEAN_INSERT
+     * 
+     * @parameter expression="type" 
+     * @required
+     */
+    private String type;
+
+    /**
+     * When true, place the entired operation in one transaction
+     * @parameter expression="transaction" default-value="false"
+     * 
+     */
+    private boolean transaction;
+
+    /**
+     * import data file
+     * 
+     * @parameter expression="${src}"
+     * @required
+     */
+    private File src;
+
+    /**
+     * src format type. Valid type are: flat, xml, csv, and dtd
+     * 
+     * @parameter expression="${format}" default-value="xml";
+     * @required
+     */
+    private String format;
+
+    public void execute()
+        throws MojoExecutionException, MojoFailureException
+    {
+        if ( skip )
+        {
+            return;
+        }
+
+        super.execute();
+        
+        try
+        {
+            IDatabaseConnection connection = createConnection();
+            try
+            {
+                Operation op = new Operation();
+                op.setFormat( format );
+                op.setSrc( src );
+                op.setTransaction( transaction );
+                op.setType( type );
+                op.execute( connection );
+            }
+            finally
+            {
+                connection.close();
+            }
+        }
+        catch ( Exception e )
+        {
+            throw new MojoExecutionException( "Error executing " + type, e );
+        }
+
+    }
+}
