a9434ca2001a4002c153159c6d00f0597c01e180
Mark Prins <mprins@users.sf.net>
2019-10-30 12:14:29 +0100

    update dependencies an snapshot version

--- src/example/src/test/data/create-table.sql
+++ src/example/src/test/data/create-table.sql
@@ -1,3 +1,3 @@
 drop table person if exists;
-create table person ( id integer, first_name varchar, last_name varchar);
+create table person ( id integer, first_name varchar(255), last_name varchar(255));
 shutdown;
